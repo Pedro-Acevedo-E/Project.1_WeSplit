210e2e3edb808bb95505b836cdfc338b3036c506
Pedro_Acevedo <acevedoensisopedrogabriel@gmail.com>
2023-03-07 10:57:10 -0800

    Initial commit

---
+++ Project.1_WeSplit/Project_1_WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_1_WeSplitApp.swift
+//  Project.1_WeSplit
+//
+//  Created by Pedro Acevedo on 07/03/23.
+//
+
+import SwiftUI
+
+@main
+struct Project_1_WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
